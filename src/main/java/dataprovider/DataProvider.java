b0ff78391d57960cc52195c4bc9227326d372781
Ashok9492 <ashokkonda43@gmail.com>
2021-05-14 10:35:28 +0530

    first commit

---
+++ src/main/java/dataprovider/DataProvider.java
@@ -0,0 +1,34 @@
+package dataprovider;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DataProvider {
+
+	public void login(String email,String Firstname,String lastname,String password) {
+		
+	
+	System.setProperty("webdriver.chrome.driver","C:\\Users\\sky\\Downloads\\chromedriver_win32\\chromedriver.exe");
+	WebDriver driver = new ChromeDriver();
+	driver.manage().timeouts().pageLoadTimeout(1, TimeUnit.MINUTES);
+	driver.manage().timeouts().implicitlyWait(1, TimeUnit.MINUTES);
+	driver.get("http://automationpractice.com/");
+	driver.findElement(By.xpath("//a[contains(text(),'Sign in')]")).click();
+	driver.findElement(By.xpath("//input[@id='email_create']")).sendKeys("rock9ashok@gmail.com");
+	driver.findElement(By.xpath("//button[@id='SubmitCreate']")).click();
+	driver.findElement(By.xpath("//input[@id='id_gender1']")).click();
+	driver.findElement(By.xpath("//input[@name='customer_firstname']")).sendKeys("Ashok");
+	driver.findElement(By.xpath("//input[@name='customer_lastname']")).sendKeys("Konda");
+	driver.findElement(By.xpath("//input[@name='passwd']")).sendKeys("9492969271");
+	
+	}
+	
+	
+	
+	
+	
+	
+}
